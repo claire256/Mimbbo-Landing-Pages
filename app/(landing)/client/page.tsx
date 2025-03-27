a7e24868b8308f40ba7715bb0290ab0e702a5ddc
claire256 <clairenabwami3@gmail.com>
2025-03-27 13:27:49 +0300

    add header and herosection

---
+++ app/(landing)/client/page.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import "../../../styles/landing-page.css"
+import Header from "../components/header";
+import HeroSection from "./components/client-hero-section";
+
+
+export default function Home() {
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      AOS.init({
+        duration: 1000,
+        once: true,
+      })
+    }
+  }, []);
+
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Header />
+
+      <main className="flex-1">
+        <HeroSection />
+
+      </main>
+    </div>
+  )
+}
+
