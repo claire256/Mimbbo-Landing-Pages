f94d5d1b678217721f642d08e4c1713258f4676f
claire256 <clairenabwami3@gmail.com>
2025-03-27 09:26:19 +0300

    replace qr code and socials

--- app/(landing)/components/cta.tsx
+++ app/(landing)/components/cta.tsx
@@ -1,41 +1,49 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
-import { Button } from "@/components/ui/button"
-import Image from 'next/image';
-import QRCode from 'react-qr-code';
-
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
 
 const CTA = () => {
-
   const webUrl = process.env.NEXT_PUBLIC_MIMBBO_WEB_URL || "";
   const signupUrl = `${webUrl}/?auth=signup`;
 
   return (
     <section className="mt-16 bg-[#6d1d14] py-12 text-white">
       <div className="max-w-5xl px-6 mx-auto text-center md:px-8">
-        <h2 className="text-2xl font-semibold rdy-text">Ready to get started?</h2>
-        <Link
-        href={signupUrl}
-        target="_blank" 
-        rel="noopener noreferrer"
-      >
-        <Button className="mt-6 rounded-md bg-white px-6 py-2 text-[#6d1d14] hover:bg-white/90 theme-btn  signup">Sign Up</Button>
-      </Link>
+        <h2 className="text-2xl font-semibold rdy-text">
+          Ready to get started?
+        </h2>
+        <Link href={signupUrl} target="_blank" rel="noopener noreferrer">
+          <Button className="mt-6 rounded-md bg-white px-6 py-2 text-[#6d1d14] hover:bg-white/90 theme-btn  signup">
+            Sign Up
+          </Button>
+        </Link>
         <div className="">
-          <p className="text-xs download">Download our mobile app to get started</p>
-          <div className="flex justify-center qr_code">
-            <Image
-              src="/qr-code.png?height=100&width=100"
-              alt="QR Code"
-              width={100}
-              height={100}
-              className="w-24 h-24"
-            />
+          <p className="text-xs download">Download the Mimmbbo App</p>
+          <div className="flex gap-4 justify-center qr_code">
+            <Link href={"https://apps.apple.com/us/app/mimbbo/id1602788926"} target="_blank" rel="noopener noreferrer">
+              <Image
+                src="/appstore.png"
+                alt="appstore"
+                width={1}
+                height={1}
+                className="apps-icon h-10 w-36 "
+              />
+            </Link>
+            <Link href={"https://play.google.com/store/apps/details?id=com.mimbbo.app"} target="_blank" rel="noopener noreferrer">
+              <Image
+                src="/playstore.png"
+                alt="playstore"
+                width={1}
+                height={1}
+                className="apps-icon h-10 w-36 "
+              />
+            </Link>
           </div>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default CTA
+export default CTA;
--- app/(landing)/components/footer.tsx
+++ app/(landing)/components/footer.tsx
@@ -1,6 +1,6 @@
-import { Facebook, Instagram, Linkedin, Twitter, } from "lucide-react"
-import Image from "next/image"
-import Link from "next/link"
+import { Facebook, Instagram, Linkedin, Twitter } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
 
 const Footer = () => {
   return (
@@ -17,35 +17,78 @@ const Footer = () => {
             />
           </div>
           <div className="flex gap-4 social_links">
-            <Link href="https://www.facebook.com/mimbbo/" target="_blank" rel="noopener noreferrer" aria-label="Facebook">
-              <Facebook className="w-5 h-5 text-muted-foreground" />
+            <Link
+              href="https://www.facebook.com/mimbbo/"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="Facebook"
+              className="bg-secondary"
+            >
+              <Image 
+              className="w-5 h-5" 
+              src="/Facebook.png"
+              alt="facebook"
+              width={300}
+              height={50}
+              />
             </Link>
-            <Link href="https://x.com/mimbbo" target="_blank" rel="noopener noreferrer" aria-label="Twitter">
-              <Twitter className="w-5 h-5 text-muted-foreground" />
+            
+            <Link
+              href="https://www.instagram.com/mimbbo/"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="Instagram"
+            >
+              <Image 
+              className="w-5 h-5" 
+              src="/Instagram.png"
+              alt="instagram"
+              width={300}
+              height={50}
+              />
             </Link>
-            <Link href="https://www.instagram.com/mimbbo/" target="_blank" rel="noopener noreferrer" aria-label="Instagram">
-              <Instagram className="w-5 h-5 text-muted-foreground" />
-            </Link>
-            <Link href="https://www.linkedin.com/company/mimbbo/" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn">
-              <Linkedin className="w-5 h-5 text-muted-foreground" />
+            <Link
+              href="https://www.linkedin.com/company/mimbbo/"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="LinkedIn"
+            >
+              <Image 
+              className="w-5 h-5" 
+              src="/Linkedin.png"
+              alt="linkedin"
+              width={300}
+              height={50}
+              />
             </Link>
           </div>
         </div>
         <div className="flex flex-col items-center justify-between gap-4 pt-6 mt-6 text-xs border-t text-muted-foreground md:flex-row">
-          <div className="copyright">Copyright © {new Date().getFullYear()} Mimbbo. All rights reserved.</div>
+          <div className="copyright">
+            Copyright © {new Date().getFullYear()} Mimbbo. All rights reserved.
+          </div>
           <div className="flex gap-4 terms">
-            <Link href="https://terms.mimbbo.com/" target="_blank" rel="noopener noreferrer" className="underline">
-              Privacy policy
+            <Link
+              href="https://support.mimbbo.com/support/solutions"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="underline"
+            >
+              Help Desk
             </Link>
-            <Link href="https://terms.mimbbo.com/" target="_blank" rel="noopener noreferrer" className="underline">
+            <Link
+              href="https://terms.mimbbo.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="underline"
+            >
               Terms of service
             </Link>
           </div>
         </div>
       </div>
     </footer>
-  )
-}
+  );
+};
 
-
-export default Footer
+export default Footer;
